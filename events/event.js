2600c7f0ae14f51c44ca0eb4a9fc42b013c241e2
IamNagas <nraj5647@gmail.com>
2021-12-21 21:19:49 +0530

    events

---
+++ events/event.js
@@ -0,0 +1,40 @@
+function add(){
+    console.log("this is add function ");
+    var a=10;
+    var b=20;
+    alert(a+b)
+    document.getElementById("one").style.backgroundColor="red";
+}
+
+function multi(){
+    console.log("multi function");
+    var a=5;
+    var b=5;
+    alert(a*b)
+    document.getElementById("two").style.backgroundColor="blue";
+
+}
+function sub(){
+    document.getElementById("three").style.backgroundColor="green"
+    console.log("sub function");
+    var a=100;
+    var b=25;
+    alert(a-b)
+    
+}
+function div(){
+    document.getElementById("four").style.backgroundColor="yellow"
+    console.log("div function");
+    var a=100;
+    var b=25;
+    alert(a%b)
+    
+}
+function best(){
+
+
+    document.getElementById("five").style.backgroundColor="black"
+}
+function success (){
+    document.getElementById("six").style.backgroundColor="pink"
+}
